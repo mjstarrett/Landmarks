f125198a7119a29ff2ec81fe131b17797730b83a
mjstarrett@ucdavis.edu <mjstarrett@WayFinder.local>
2018-04-27 11:29:03 -0700

    environment updates 04/27

---
+++ Assets/navigateByStarr_lite/scripts/eventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class eventManager : MonoBehaviour {
+
+	private Dictionary <string, UnityEvent> eventDictionary;
+
+	private eventManager eventManager;
+
+	public static eventManager instance{
+		get{
+		
+		}
+	}
+
+}
