2397e66b18749187780e5b3977b1b76e27914e64
mjstarrett <15330251+mjstarrett@users.noreply.github.com>
2019-01-08 17:36:36 -0700

    Scaled player instantiates and moves - TODO fix looking and mvmt speed

--- Assets/Landmarks2018/scripts/InitializeScaledNavTask.cs
+++ Assets/Landmarks2018/scripts/InitializeScaledNavTask.cs
@@ -7,6 +7,9 @@ public class InitializeScaledNavTask : ExperimentTask
 {
 
     public GameObject scaledEnvironment;
+    public GameObject startLocationsParent; // must have at least 1 child
+    public bool randomStartLocation = false;
+    public float scaledPlayerSpeed;
 
     public override void startTask()
     {
@@ -19,29 +22,55 @@ public class InitializeScaledNavTask : ExperimentTask
         if (!manager) Start();
         base.startTask();
 
-        //// make the cursor functional and visible
-        //Cursor.lockState = CursorLockMode.None;
-        //Cursor.visible = true;
+        //---------------------------------------------
+        // Adjust basic parameters for task
+        //---------------------------------------------
 
-        //// Turn off Player movement
-        //avatar.GetComponent<CharacterController>().enabled = false;
-
+        // make the cursor functional and visible
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        // Turn off Player movement
+        avatar.GetComponent<CharacterController>().enabled = false;
         // Make the scaled environment visible
         scaledEnvironment.SetActive(true);
 
-        //// Copy the player from Experiment so it functions the same way
-        //GameObject ScaledPlayer = Instantiate<GameObject>(avatar, transform.parent);
-        //ScaledPlayer.name = "ScaledPlayer";
-        //// Adjust the scale
 
-    //    // Swap from 1st-person camera to overhead view
-    //    firstPersonCamera.enabled = false;
-    //    overheadCamera.enabled = true;
+        //---------------------------------------------
+        // Select from starting locations in scaled Env.
+        //---------------------------------------------
 
-    //    // Change text and turn on the map action button
-    //    actionButton.GetComponentInChildren<Text>().text = "Continue to Test";
-    //    manager.actionButton.SetActive(true);
-    //    actionButton.onClick.AddListener(OnActionClick);
+        //------FIXME Handle list of start locations-------- 
+        //// Select a starting location for the scaled player
+        //if (startLocationsParent.transform.childCount != 0)
+        //{
+        //    List<GameObject> startLocations = new List<GameObject>();
+        //    foreach (Transform child in startLocationsParent.transform)
+        //    {
+        //        startLocations.Add(child.gameObject);
+        //    }
+        //}
+
+
+        //---------------------------------------------
+        // Instantiate the scaled 1st-person controller
+        //---------------------------------------------
+
+        // Copy the player from Experiment so it functions the same way
+        GameObject scaledPlayer = Instantiate<GameObject>(avatar, startLocationsParent.transform.position, startLocationsParent.transform.rotation, scaledEnvironment.transform);
+        scaledPlayer.name = "ScaledPlayer";
+        scaledPlayer.GetComponent<CharacterController>().enabled = true;
+        //FIXME Figure out how to make the scaled up player move faster
+        //scaledPlayer.GetComponent<CapsuleCollider>().attachedRigidbody.mass = scaledPlayer.GetComponent<CapsuleCollider>().attachedRigidbody.mass * scaledPlayerSpeed;
+       
+
+        //    // Swap from 1st-person camera to overhead view
+        //    firstPersonCamera.enabled = false;
+        //    overheadCamera.enabled = true;
+
+        //    // Change text and turn on the map action button
+        //    actionButton.GetComponentInChildren<Text>().text = "Continue to Test";
+        //    manager.actionButton.SetActive(true);
+        //    actionButton.onClick.AddListener(OnActionClick);
     }
 
     public override bool updateTask()
