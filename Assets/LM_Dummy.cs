51d9a926246e87cb069bd804a8cdab4ba6c28a76
mjstarrett <15330251+mjstarrett@users.noreply.github.com>
2019-01-18 15:05:54 -0700

    automated the functionality of <TaskList>().tasks

---
+++ Assets/LM_Dummy.cs
@@ -0,0 +1,53 @@
+﻿/*
+    LM Dummy
+       
+    Attached object holds task components that need to be effectively ignored 
+    by Tasklist but are required for the script. Thus the object this is 
+    attached to can be detected by Tasklist (won't throw error), but does nothing 
+    except start and end.   
+
+    Copyright (C) 2019 Michael J. Starrett
+
+    Navigate by StarrLite (Powered by LandMarks)
+    Human Spatial Cognition Laboratory
+    Department of Psychology - University of Arizona   
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LM_Dummy : ExperimentTask
+{
+
+    public override void startTask()
+    {
+        TASK_START();
+    }
+
+
+    public override void TASK_START()
+    {
+        if (!manager) Start();
+        base.startTask();
+    }
+
+
+    public override bool updateTask()
+    {
+        return true;
+    }
+
+
+    public override void endTask()
+    {
+        TASK_END();
+    }
+
+
+    public override void TASK_END()
+    {
+        base.endTask();
+    }
+
+}
