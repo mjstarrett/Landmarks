c17185501549da965f80075b60b3f8f0d520fdd4
mjstarrett <15330251+mjstarrett@users.noreply.github.com>
2019-02-04 14:07:10 -0700

    fixed errors with io port config namespaces

--- Assets/EEG Assets/TriggerBox_sendPulse.cs
+++ Assets/EEG Assets/TriggerBox_sendPulse.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
 using System.IO.Ports;
 using UnityEngine;
 
@@ -35,12 +39,12 @@ public class Pulses : MonoBehaviour {
     public void initiatePort()
     {
         var config = Config.instance;
-        sPort = new SerialPort(config.portName, 19200, Parity.None, 8, StopBits.One);
+        sPort = new SerialPort(config.name, 19200, Parity.None, 8, StopBits.One); // had to change config.portName to config.Name (no definition for portName in Config)
         sPort.Open();
         // Set the port to an initial state
         data[0] = 0x00;
         sPort.Write(data, 0, 1);
-        Debug.Log("port open! " + config.portName);
+        Debug.Log("port open! " + config.name); // had to change config.portName to config.Name (no definition for portName in Config)
     }
 
     public void closePort()
