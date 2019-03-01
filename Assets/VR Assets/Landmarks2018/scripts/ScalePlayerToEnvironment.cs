a7d8f73f32cece2fa0cc729211c1aafd500a6753
Michael Starrett <mjstarrett@mbb.arizona.edu>
2019-03-01 12:51:28 -0700

    fixed custom editor build bug

--- Assets/VR Assets/Landmarks2018/scripts/ScalePlayerToEnvironment.cs
+++ Assets/VR Assets/Landmarks2018/scripts/ScalePlayerToEnvironment.cs
@@ -126,31 +126,3 @@ public class ScalePlayerToEnvironment : ExperimentTask
 }
 
 
-// Custom Inspector to handle sufficient conditions
-[CustomEditor(typeof(ScalePlayerToEnvironment))]
-[CanEditMultipleObjects]
-public class ScalePlayerCustomEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        // set our custom inspector up to communicate with the main script and alias that script for brevity
-        ScalePlayerToEnvironment spte = (ScalePlayerToEnvironment)target;
-    
-        // Gameobject field for Scaled environment
-        spte.scaledEnvironment = (GameObject)EditorGUILayout.ObjectField("Scaled Enviornment", spte.scaledEnvironment, typeof(GameObject), true);
-
-        // Gameobject field for StartLocationsParent
-        spte.startLocationsParent = (GameObject)EditorGUILayout.ObjectField("Start Locations Parent", spte.startLocationsParent, typeof(GameObject), true);
-
-        // bool for randomizing start location
-        spte.randomStartLocation = EditorGUILayout.Toggle("Random Start Location", spte.randomStartLocation);
-
-        // publish our autoscale option
-        spte.autoscale = EditorGUILayout.Toggle("AutoScale", spte.autoscale);
-        // tell inspector to hide the scale ratio option if they are autoscaling
-        using (new EditorGUI.DisabledScope(spte.autoscale)) 
-        {
-            spte.scaleRatio = EditorGUILayout.FloatField("Scale Ratio", spte.scaleRatio);
-        }
-    }
-}
