457d8749cce7aabc6fe664fe96d2333bb4235c16
Michael Starrett <mjstarrett@mbb.arizona.edu>
2019-03-26 12:28:00 -0700

    beginning to work on pointer and ui collider interactions

---
+++ Assets/VR Assets/Landmarks2018/scripts/VR_UI_Input.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR;
+
+[RequireComponent(typeof(SteamVR_LaserPointer_mjs))]
+public class VR_UI_Input : MonoBehaviour
+{
+    private SteamVR_LaserPointer_mjs laserPointer;
+    private SteamVR_Action_Boolean interactUI = SteamVR_Input.GetBooleanAction("Interact UI");
+
+    private void OnEnable()
+    {
+        laserPointer = GetComponent<SteamVR_LaserPointer_mjs>();
+    }
+
+    private void HandleTriggerClicked(object sender, PointerEventArgs e)
+    {
+        if (EventSystem.current.currentSelectedGameObject != null)
+        {
+            ExecuteEvents.Execute(EventSystem.current.currentSelectedGameObject, new PointerEventData(EventSystem.current), ExecuteEvents.submitHandler);
+        }
+    }
+
+    private void HandlePointerIn(object sender, PointerEventArgs e)
+    {
+        var button = e.target.GetComponent<Button>();
+        if (button != null)
+        {
+            button.Select();
+            Debug.Log("HandlePointerIn", e.target.gameObject);
+        }
+    }
+
+    private void HandlePointerOut(object sender, PointerEventArgs e)
+    {
+
+        var button = e.target.GetComponent<Button>();
+        if (button != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            Debug.Log("HandlePointerOut", e.target.gameObject);
+        }
+    }
+}
---
+++ Assets/VR Assets/Landmarks2018/scripts/VR_UI_Item.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class VR_UI_Item : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+    private RectTransform rectTransform;
+
+    private void OnEnable()
+    {
+        ValidateCollider();
+    }
+
+    private void OnValidate()
+    {
+        ValidateCollider();
+    }
+
+    private void ValidateCollider()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        boxCollider = GetComponent<BoxCollider>();
+        if (boxCollider == null)
+        {
+            boxCollider = gameObject.AddComponent<BoxCollider>();
+        }
+
+        boxCollider.size = rectTransform.sizeDelta;
+    }
+}
