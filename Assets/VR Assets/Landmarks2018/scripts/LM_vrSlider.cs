08b8fe72367846f7a4deb518ea9d9d775230c575
Michael Starrett <mjstarrett@mbb.arizona.edu>
2019-04-01 13:25:22 -0700

    mid-day save 04/01/2019

---
+++ Assets/VR Assets/Landmarks2018/scripts/LM_vrSlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LM_vrSlider : MonoBehaviour
+{
+
+    public GameObject start;
+    public GameObject end;
+    public GameObject handle;
+    public GameObject fill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
