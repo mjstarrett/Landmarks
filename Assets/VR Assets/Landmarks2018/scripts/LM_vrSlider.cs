9b09ec1848234e2803fcbca4b8daaadf956c83ae
Michael Starrett <mjstarrett@mbb.arizona.edu>
2019-04-02 10:33:26 -0700

    slider snaps working, but attach location is buggy

--- Assets/VR Assets/Landmarks2018/scripts/LM_vrSlider.cs
+++ Assets/VR Assets/Landmarks2018/scripts/LM_vrSlider.cs
@@ -7,8 +7,8 @@ public class LM_vrSlider : MonoBehaviour
     public GameObject handle;
     public GameObject start;
     public GameObject end;
-    private float minZ = 0;
-    public float maxZ = 3;
+    private float min = 0;
+    public float max = 3;
     public bool wholeNumbers = false;
 
     private float totalZ;
@@ -18,14 +18,18 @@ public class LM_vrSlider : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Set up the anchor on the handle to be based on the far left of the slider (so it's displacement equals our value)
+        handle.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0.5f);
+        handle.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0.5f);
+
 
         totalZ = Mathf.Abs(end.GetComponent<RectTransform>().anchoredPosition.x - start.GetComponent<RectTransform>().anchoredPosition.x);
 
-        anchorPoints = new float[(int)maxZ + 1]; // handle the fact that zero is one of our snaps
+        anchorPoints = new float[(int)max + 1]; // handle the fact that zero is one of our snaps
 
-        for (int iSnap = (int)minZ; iSnap < maxZ+1; iSnap++)
+        for (int iSnap = (int)min; iSnap < max+1; iSnap++)
         {
-            anchorPoints[iSnap] = iSnap * (totalZ / (int)maxZ);
+            anchorPoints[iSnap] = iSnap * (totalZ / (int)max);
         }
 
         Vector3 position = handle.GetComponent<RectTransform>().anchoredPosition;
@@ -36,23 +40,29 @@ public class LM_vrSlider : MonoBehaviour
     public void GetSnap()
 {
 		SnapToPosition();
+        Debug.Log("Checking Snap Position");
 }
 
 
     public void SnapToPosition()
     {
-
+        Debug.Log("Roger that... checking now");
         //Cycle through each predefined anchor point
         for (int i = 0; i < anchorPoints.Length; i++)
         {
             //If lever is within "snapping distance" of that anchor point
-            if (Mathf.Abs(handle.GetComponent<RectTransform>().anchoredPosition.x - anchorPoints[i]) < snapDistance)
+            if (Mathf.Abs(handle.GetComponent<RectTransform>().anchoredPosition.x - anchorPoints[i]) < totalZ/(anchorPoints.Length + 1))
             {
                 //Get current lever position and update z pos to anchor point
-                float snapPosition = handle.GetComponent<RectTransform>().anchoredPosition.x;
+                Vector3 snapPosition = handle.GetComponent<RectTransform>().anchoredPosition;
+                snapPosition.x = anchorPoints[i];
+                handle.GetComponent<RectTransform>().anchoredPosition = snapPosition;
+                Debug.Log("Did we snap to " + snapPosition.x);
                 //Break so it stops checking other anchor points
                 break;
             }
         }
+
+       
     }
 }
