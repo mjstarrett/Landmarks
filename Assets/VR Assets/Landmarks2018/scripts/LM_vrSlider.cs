8b4afbbad33dd0c3464bf37c8acc53130721aba7
Michael Starrett <mjstarrett@mbb.arizona.edu>
2019-04-01 17:29:18 -0700

    major edits needed to get slider working; see LM_vrSlider.cs

--- Assets/VR Assets/Landmarks2018/scripts/LM_vrSlider.cs
+++ Assets/VR Assets/Landmarks2018/scripts/LM_vrSlider.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class LM_vrSlider : MonoBehaviour
 {
-
+    public GameObject handle;
     public GameObject start;
     public GameObject end;
-    public GameObject handle;
-    public GameObject fill;
+    private float minZ = 0;
+    public float maxZ = 3;
+    public bool wholeNumbers = false;
+
+    private float totalZ;
+    public float[] anchorPoints;
+    public float snapDistance = 0.05f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        totalZ = Mathf.Abs(end.GetComponent<RectTransform>().anchoredPosition.x - start.GetComponent<RectTransform>().anchoredPosition.x);
+
+        anchorPoints = new float[(int)maxZ + 1]; // handle the fact that zero is one of our snaps
+
+        for (int iSnap = (int)minZ; iSnap < maxZ+1; iSnap++)
+        {
+            anchorPoints[iSnap] = iSnap * (totalZ / (int)maxZ);
+        }
+
+        Vector3 position = handle.GetComponent<RectTransform>().anchoredPosition;
+        position.x = anchorPoints[0];
+        handle.GetComponent<RectTransform>().anchoredPosition = position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetSnap()
+{
+		SnapToPosition();
+}
+
+
+    public void SnapToPosition()
     {
-        
+
+        //Cycle through each predefined anchor point
+        for (int i = 0; i < anchorPoints.Length; i++)
+        {
+            //If lever is within "snapping distance" of that anchor point
+            if (Mathf.Abs(handle.GetComponent<RectTransform>().anchoredPosition.x - anchorPoints[i]) < snapDistance)
+            {
+                //Get current lever position and update z pos to anchor point
+                float snapPosition = handle.GetComponent<RectTransform>().anchoredPosition.x;
+                //Break so it stops checking other anchor points
+                break;
+            }
+        }
     }
 }
