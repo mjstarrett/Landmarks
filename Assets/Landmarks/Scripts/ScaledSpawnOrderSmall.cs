d99d3833c60c43388d1ac54634948d3f1bed74b4
Michael Starrett <mjstarrett@mbb.arizona.edu>
2019-05-03 12:36:43 -0700

    major issues with VR spawn movements for large vs small avatar

---
+++ Assets/Landmarks/Scripts/ScaledSpawnOrderSmall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScaledSpawnOrderSmall : MonoBehaviour 
+{
+
+//	 SEE ALSO TransformOrder and ObjectOrder
+
+	[HideInInspector]
+	public List<GameObject> scaledSpawnOrderSmall; // accessed later (StoreList and subjsequent), but not visible
+
+	void Awake()
+	{
+
+		foreach (Transform child in this.transform) 
+		{
+			scaledSpawnOrderSmall.Add (GameObject.Find(child.name));
+			string tmp = child.name;
+			Debug.Log ((child.name));
+		}			
+
+	}
+}
