4db4e19307addeac43cde7992e37e81ca63812c7
mjstarrett <15330251+mjstarrett@users.noreply.github.com>
2019-05-07 14:56:03 -0700

    really stuck on catch trials for main task loop ESC_ModifiedNavTask

---
+++ Assets/CatchTrials.cs
@@ -0,0 +1,55 @@
+﻿/*  CatchTrials
+
+    Used to set up subsequent loops where various modular components are to be
+    skipped, such as catch trials where perhaps a cue never comes or a test 
+    never follows as is typical with other trials in that loop.
+
+    Copyright (C) 2019 Michael J. Starrett
+
+    Navigate by StarrLite (Powered by LandMarks)
+    Human Spatial Cognition Laboratory
+    Department of Psychology - University of Arizona   
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchTrials : ExperimentTask
+{
+    // Public Variables
+    public TaskList targetLoop;
+    public GameObject[] skipOnCatch;
+    public int catchFlagsPerLoop; // how many trials should get flagged?
+
+
+    public override void startTask()
+    {
+        TASK_START();
+    }
+
+
+    public override void TASK_START()
+    {
+        if (!manager) Start();
+        base.startTask();
+    }
+
+
+    public override bool updateTask()
+    {
+        return true;
+    }
+
+
+    public override void endTask()
+    {
+        TASK_END();
+    }
+
+
+    public override void TASK_END()
+    {
+        base.endTask();
+    }
+}
