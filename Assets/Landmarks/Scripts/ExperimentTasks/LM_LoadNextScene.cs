6fdf7dc153df1bd7ae3a944d4d1bb95777c52405
Michael Starrett <mjstarrett@mbb.arizona.edu>
2019-06-27 14:18:19 -0700

    updates to functionality. New ESC build debugged

--- Assets/Landmarks/Scripts/ExperimentTasks/LM_LoadNextScene.cs
+++ Assets/Landmarks/Scripts/ExperimentTasks/LM_LoadNextScene.cs
@@ -20,57 +20,52 @@ public class LM_LoadNextScene : ExperimentTask
         if (!manager) Start();
         base.startTask();
 
-
-        // Grab any info about loading next scenes or conditions from PlayerPrefs(X)
-        var nextIndex = PlayerPrefs.GetInt("NextIndex");
-        var nextLevels = PlayerPrefsX.GetStringArray("NextLevels");
-        var nextConditions = PlayerPrefsX.GetStringArray("NextConditions");
-
-        // If we have a next task/condition, update it
-        if (nextIndex < nextLevels.Length)
+        if (!skip)
         {
-            manager.config.level = nextLevels[nextIndex];
-            var levelname = manager.config.level; // save from destruction
-            manager.config.condition = nextConditions[nextIndex];
 
-            nextIndex++;
-            PlayerPrefs.SetInt("NextIndex", nextIndex);
+            // Grab any info about loading next scenes or conditions from PlayerPrefs(X)
+            var nextIndex = PlayerPrefs.GetInt("NextIndex");
+            var nextLevels = PlayerPrefsX.GetStringArray("NextLevels");
+            var nextConditions = PlayerPrefsX.GetStringArray("NextConditions");
 
-
-            // Handle the current Landmarks structure to avoid breaking the game on loading the next scene
-            if (avatar.GetComponent<CVirtHapticListener>() != null)
+            // If we have a next task/condition, update it
+            if (nextIndex < nextLevels.Length)
             {
-                Destroy(avatar.GetComponent<CVirtHapticListener>()); // There can only be one!
-            }
-            GameObject oldInstance = GameObject.Find("_Landmarks_"); 
-            oldInstance.name = "OldInstance";
-            GameObject.FindWithTag("Experiment").SetActive(false);
-            GameObject.FindWithTag("Environment").SetActive(false);
-            Destroy(avatar); // particularly important for SteamVR and interaction system; bugs on load
+                manager.config.level = nextLevels[nextIndex];
+                var levelname = manager.config.level; // save from destruction
+                manager.config.condition = nextConditions[nextIndex];
 
-            // avoid frame-drop during load forcing to SteamVr compositor by using SteamVR_LoadLevel for VR apps
-            if (vrEnabled)
-            {
-                SteamVR_LoadLevel.Begin(levelname);
-                Debug.Log("Loading new VR scene");
-            }
-            else SceneManager.LoadScene(levelname); // otherwise, just load the level like usual
+                nextIndex++;
+                PlayerPrefs.SetInt("NextIndex", nextIndex);
 
-        }
-        else
-        {
-            this.skip = true;
+
+                // Handle the current Landmarks structure to avoid breaking the game on loading the next scene
+                if (avatar.GetComponent<CVirtHapticListener>() != null)
+                {
+                    Destroy(avatar.GetComponent<CVirtHapticListener>()); // There can only be one!
+                }
+                GameObject oldInstance = GameObject.Find("_Landmarks_");
+                oldInstance.name = "OldInstance";
+                GameObject.FindWithTag("Experiment").SetActive(false);
+                GameObject.FindWithTag("Environment").SetActive(false);
+                Destroy(avatar); // particularly important for SteamVR and interaction system; bugs on load
+
+                // avoid frame-drop during load forcing to SteamVr compositor by using SteamVR_LoadLevel for VR apps
+                if (vrEnabled)
+                {
+                    SteamVR_LoadLevel.Begin(levelname);
+                    Debug.Log("Loading new VR scene");
+                }
+                else SceneManager.LoadScene(levelname); // otherwise, just load the level like usual
+
+            }
         }
     }
 
 
     public override bool updateTask()
     {
-        if (skip)
-        {
-            //log.log("INFO    skip task    " + name,1 );
-            return true;
-        }
+        
         return true;
     }
 
