8ba9492d469c00a05a72eac9b6127783866bb4fd
mjstarrett <15330251+mjstarrett@users.noreply.github.com>
2019-08-28 11:36:51 -0700

    created and configured ViveKatwalkController.prefab; need to integrate into landmarks experiment.cs and test in 215a

---
+++ Assets/Proprietary/KATVR SDK/Scripts/KATVR.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+namespace KATVR
+{
+    public static class KATVR_Basic {
+        #region Basic Variable - 基础变量
+        public enum LanguageList { Chinese, Engligh };
+        public static LanguageList Language;
+        public static string LanguageFilePath = Application.dataPath + "/LanguageFile.xml";
+        #endregion
+    }
+
+    public class KATVR_Global
+    {
+        public static KATDevice_Walk KDevice_Walk;
+    }
+
+}
+
