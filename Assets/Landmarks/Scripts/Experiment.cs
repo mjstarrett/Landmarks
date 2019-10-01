51fd532d1f44d26dd339f87a2325bf0c900fc31b
mjstarrett <mjstarrett@gmail.com>
2019-10-01 08:08:16 -0700

    Revert "organize LM prefab hierarchy"
    This reverts commit 14a1d0f27804c25cbaf724b4f70798ca7dee2625.

--- Assets/Landmarks/Scripts/Experiment.cs
+++ Assets/Landmarks/Scripts/Experiment.cs
@@ -32,19 +32,14 @@ public enum EndListMode
 [SerializeField]
 public enum UserInterface
 {
-    DesktopDefaultController,
-    ViveRoomspaceController,
-    ViveVirtualizerController,
-    ViveKatwalkController
-    
+	DesktopDefault,
+	ViveRoomspace,
+	ViveVirtualizer
 }
-
 
 public class Experiment : MonoBehaviour {
 
-    public GameObject uiParent;
-    public UserInterface controllerOptions;
-    [HideInInspector] public string userInterface;
+	public UserInterface userInterface = UserInterface.DesktopDefault;
     [HideInInspector] public TaskList tasks;
 	[HideInInspector] public Config config;
 	private long microseconds = 1;
@@ -62,13 +57,13 @@ public class Experiment : MonoBehaviour {
     [HideInInspector] public bool usingVR;
 
 	[HideInInspector] public dbLog dblog;
-	
-	
+
+
 	private bool playback = false;
 	private bool pause = true;
 	private bool done = false;
 	private long now;
-	
+
 	private Event evt;
 	private long playback_start;
 	private long playback_offset;
@@ -77,17 +72,17 @@ public class Experiment : MonoBehaviour {
 	private long next_time;
 	private string[] next_action;
 	//private long playback_start;
-	
+
 	protected GameObject avatar;
 	protected AvatarController avatarController;
 	protected HUD hud;
 
 	public static long Now() {
-		
+
 		long tick = DateTime.Now.Ticks;
         return tick / TimeSpan.TicksPerMillisecond;
 	}
-	
+
 	void Awake() {
 
 
@@ -122,11 +117,10 @@ public class Experiment : MonoBehaviour {
 
         // Assign the scaled player if it's in the scene
         scaledPlayer = GameObject.Find("SmallScalePlayerController");
-        scaledPlayer.SetActive(false);
 
         // find Environment
         environment = GameObject.FindGameObjectWithTag("Environment");
-        
+
 
         // Handle the selected UI enum from the inspector
         if (PlayerPrefs.GetString("UserInterface") != "default")
@@ -138,8 +132,8 @@ public class Experiment : MonoBehaviour {
                 case "Desktop":
                     userInterface = "DesktopDefaultController";
                     break;
-                case "ViveVirtualizer":
-                    userInterface = "ViveVirtualizerController";
+                case "Vive Virt.":
+                    userInterface = UserInterface.ViveVirtualizer;
                     break;
                 case "ViveRoomspace":
                     userInterface = "ViveRoomspaceController";
@@ -183,7 +177,7 @@ public class Experiment : MonoBehaviour {
 
                 break;
 
-            case "ViveVirtualizerController":
+            case UserInterface.ViveVirtualizer:
 
                 // This is a proprietary asset that must be added to the _Landmarks_ prefab to work
                 // If it is not added (either for lack of need or lack of the proprietary SDK), use the default
@@ -244,7 +238,7 @@ public class Experiment : MonoBehaviour {
 		playerCamera.enabled = true;
 		overheadCamera.enabled = false;
 		Cursor.visible = false;
-		
+
 
 		// Set the avatar and hud
 		avatar = player;
@@ -258,7 +252,7 @@ public class Experiment : MonoBehaviour {
 
         logfile = config.subjectPath + "/" + PlayerPrefs.GetString("expID") + "_" + config.subject + "_" + config.level + ".log";
 		configfile = config.expPath + "/" + config.filename;
-		
+
 		//when in editor
 		if (!config.bootstrapped) {
 			logfile = Directory.GetCurrentDirectory() + "/data/tmp/" + "test.log";
@@ -277,14 +271,14 @@ public class Experiment : MonoBehaviour {
 
         //start session
 
-        dblog.log("EXPERIMENT:\t" + PlayerPrefs.GetString("expID") + "\tSUBJECT:\t" + config.subject + 
+        dblog.log("EXPERIMENT:\t" + PlayerPrefs.GetString("expID") + "\tSUBJECT:\t" + config.subject +
                   "\tSTART_SCENE\t" + config.level + "\tSTART_CONDITION:\t" + config.condition + "\tUI:\t" + userInterface, 1);
 
         Debug.Log(XRSettings.loadedDeviceName);
     }
 
 
-    public void StartPlaying() {		
+    public void StartPlaying() {
 		long tick = DateTime.Now.Ticks;
         playback_start = tick / TimeSpan.TicksPerMillisecond;
         playback_offset = 0;
@@ -296,12 +290,12 @@ public class Experiment : MonoBehaviour {
 		hud.showFPS = config.showFPS;
 		hud.showTimestamp = (config.runMode == ConfigRunMode.PLAYBACK);
 
-		
-		
+
+
 		//start experiment
 		if (config.runMode != ConfigRunMode.PLAYBACK) {
 			Debug.Log ("Trying to start a task");
-			tasks.startTask();	
+			tasks.startTask();
 		} else {
 			hud.flashStatus( "Playback Paused" );
 			next_action = dblog.NextAction();
@@ -320,43 +314,43 @@ public class Experiment : MonoBehaviour {
         }
         catch
         {
-            scaledEnvironment = null; 
+            scaledEnvironment = null;
         }
 
     }
-	
+
 	public void OnControllerColliderHit(GameObject hit)  {
 		if (config.runMode != ConfigRunMode.PLAYBACK) {
 			tasks.OnControllerColliderHit(hit);
 		}
 	}
-	
+
 	void Update () {
-		
+
 		if ( !done) {
 			if (config.runMode != ConfigRunMode.PLAYBACK) {
-				
+
 				if (Input.GetKeyDown (KeyCode.T)) {
 					dblog.log("BOOKMARK	t-trigger",1 );
 				}
-				
-				done = tasks.updateTask();	
+
+				done = tasks.updateTask();
 				if (done) {
 					Cursor.visible = true;
 					Application.Quit();
-				}		
+				}
 			} else {
 				updatePlayback();
 			}
 		}
 	}
-	
+
 	void updatePlayback() {
-		
+
 		long last_now = now;
 		long tick = DateTime.Now.Ticks;
         now = tick / TimeSpan.TicksPerMillisecond;
-		
+
 		if (Input.GetButtonDown("PlayPause")) {
 			pause = !pause;
 			hud.flashStatus( "Playback Paused" );
@@ -365,44 +359,44 @@ public class Experiment : MonoBehaviour {
 		if (pause) {
 			playback_offset -= now - last_now;
 		}
-		
+
 		float seek = Input.GetAxis("Horizontal");
 		//if (seek != 0.0) {
 		if (Input.GetButton("Horizontal")) {
-			playback_offset += 250;// * Convert.ToInt64(seek);	
+			playback_offset += 250;// * Convert.ToInt64(seek);
 		}
         playback_time = now - playback_start + playback_offset;
 		hud.playback_time = playback_time;
-		
+
 		string[] vec;
 		Vector3 vec3;
-		
+
 		while ( (!pause || (seek != 0.0) ) && !done && dblog.PlaybackTime() <= playback_time  ) {
 //					 Debug.Log(dblog.PlaybackTime() + " : " + playback_time);
 								Debug.Log(next_action[2] );
-								
-								
-	//try {							
+
+
+	//try {
 			if(next_action[2] == "AVATAR_HPR" || next_action[2] == "AVATAR_POS" || next_action[2] == "AVATAR_STOP") {
-				vec = next_action[3].Split(new char[] {',', '(', ')', ' '},StringSplitOptions.RemoveEmptyEntries);				 
+				vec = next_action[3].Split(new char[] {',', '(', ')', ' '},StringSplitOptions.RemoveEmptyEntries);
 				vec3 = new Vector3(float.Parse(vec[0]), float.Parse(vec[1]), +float.Parse(vec[2]) );
-				Type t = typeof(AvatarController); 
+				Type t = typeof(AvatarController);
 				t.InvokeMember(next_action[2], BindingFlags.Default | BindingFlags.InvokeMethod, null, avatarController, new System.Object[] {vec3});
 			} else if(next_action[2] == "TASK_ROTATE" || next_action[2] == "TASK_POSITION"  || next_action[2] == "TASK_SCALE") {
 
-				vec = next_action[5].Split(new char[] {',', '(', ')', ' '},StringSplitOptions.RemoveEmptyEntries);				 
+				vec = next_action[5].Split(new char[] {',', '(', ')', ' '},StringSplitOptions.RemoveEmptyEntries);
 				vec3 = new Vector3(float.Parse(vec[0]), float.Parse(vec[1]), +float.Parse(vec[2]) );
-				
+
 				GameObject taskObject = GameObject.Find(next_action[3]);
 				Component script = taskObject.GetComponent(next_action[4]) as Component;
-				//Type t = typeof(AvatarController); 
+				//Type t = typeof(AvatarController);
 				this.GetType().InvokeMember(next_action[2], BindingFlags.Default | BindingFlags.InvokeMethod, null, this, new System.Object[] {taskObject, vec3});
-			} else if ( next_action[2] == "TASK_ADD" ) {				
+			} else if ( next_action[2] == "TASK_ADD" ) {
 				GameObject taskObject = GameObject.Find(next_action[3]);
 				Component script = taskObject.GetComponent(next_action[4]) as Component;
-				//Type t = typeof(AvatarController); 
+				//Type t = typeof(AvatarController);
 				GameObject secondObject = GameObject.Find(next_action[5]);
-				script.GetType().InvokeMember(next_action[2], BindingFlags.Default | BindingFlags.InvokeMethod, null, script, new System.Object[] {secondObject,next_action[6]});								
+				script.GetType().InvokeMember(next_action[2], BindingFlags.Default | BindingFlags.InvokeMethod, null, script, new System.Object[] {secondObject,next_action[6]});
 			} else if ( next_action[2] == "INPUT_EVENT" ) {
 				hud.flashStatus( "Input: " + next_action[3] + " " + next_action[4]);
 			} else if ( next_action[2] == "SET_SCORE" ) {
@@ -422,36 +416,36 @@ public class Experiment : MonoBehaviour {
 				GameObject taskObject = GameObject.Find(next_action[3]);
 				Component script = taskObject.GetComponent(next_action[4]) as Component;
 
-				//Type t = typeof(AvatarController); 
+				//Type t = typeof(AvatarController);
 				script.GetType().InvokeMember(next_action[2], BindingFlags.Default | BindingFlags.InvokeMethod, null, script, null);
 			}
-			
+
 	//	} catch (FormatException){
 	//	} catch (IndexOutOfRangeException){ }
-					
+
 			next_action = dblog.NextAction();
 			if (next_action == null) {
 				hud.setMessage( "Playback Done" );
-				done = true;	
+				done = true;
 			}
 
 		}
 	}
 	public  void TASK_ROTATE (GameObject go, Vector3 hpr) {
 		go.transform.localEulerAngles = hpr;
-	}	
+	}
 	public  void TASK_POSITION (GameObject go, Vector3 pos) {
 		go.transform.position = pos;
-	}	
+	}
 	public  void TASK_SCALE (GameObject go, Vector3 scale) {
 		go.transform.localScale = scale;
-	}	
+	}
 
 //log.log("TASK_POSITION\t" + current.name  + "\t" + this.GetType().Name + "\t" + current.transform.position.ToString("f4"),1);
 //		log.log("TASK_ROTATE\t" + current.name  + "\t" + this.GetType().Name + "\t" + current.transform.localRotation.ToString("f3"),1);
 //		log.log("TASK_SCALE\t" + current.name  + "\t" + this.GetType().Name + "\t" + current.transform.localScale.ToString("f3"),1);
-				
-	void OnApplicationQuit() 
+
+	void OnApplicationQuit()
 	{
 		if (config.runMode != ConfigRunMode.PLAYBACK) {
 			tasks.endTask();
@@ -459,14 +453,14 @@ public class Experiment : MonoBehaviour {
 		dblog.close();
 		Cursor.visible = true;
 	}
-	
-	void OnGUI () 
+
+	void OnGUI ()
 	{
-		
+
 		//GUILayout.Label(config.home);
 		//if (config.bootstrapped)
 		//	GUILayout.Label("BOOTSTRAPPED");
-			
+
 		/*	this captures too many events - maybe just KeyUp?
 		evt = Event.current;
 
@@ -481,7 +475,7 @@ public class Experiment : MonoBehaviour {
 		}
 		*/
 	}
-	
+
 	public static void Shuffle<T>(T[] array)
     {
 		var random = new System.Random();
@@ -510,5 +504,5 @@ public class Experiment : MonoBehaviour {
 			array[i - 1] = tmp;
 		}
 	}
-	
+
 }
