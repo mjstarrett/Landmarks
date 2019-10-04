09c4997cefbeb912f96f822dc567d023a1f72653
mjstarrett <15330251+mjstarrett@users.noreply.github.com>
2019-10-04 11:37:05 -0700

    surpressed immobilized character controller move attemp error

--- Assets/Free Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Free Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -125,7 +125,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
             {
                 m_MoveDir += Physics.gravity*m_GravityMultiplier*Time.fixedDeltaTime;
             }
-            m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
+
+            // MJS 10/2019 - checking if controller is enabled 
+            if (m_CharacterController.enabled)
+            {
+                m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
+            }
+            
 
             ProgressStepCycle(speed);
             UpdateCameraPosition(speed);
