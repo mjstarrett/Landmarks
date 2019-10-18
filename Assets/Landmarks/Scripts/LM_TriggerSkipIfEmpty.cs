be434c5ee385b338cc87b356f0390e8305eea5fa
mjstarrett <15330251+mjstarrett@users.noreply.github.com>
2019-10-18 11:29:20 -0700

    move mapTestPlaceholder generation to TASK_mappit, no longer part of main _Landmarks_ prefab

---
+++ Assets/Landmarks/Scripts/LM_TriggerSkipIfEmpty.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LM_TriggerSkipIfEmpty : MonoBehaviour
+{
+
+    public ExperimentTask taskToSkip;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (transform.childCount == 0)
+        {
+            taskToSkip.skip = true;
+        }
+        else taskToSkip.skip = false;
+    }
+
+}
