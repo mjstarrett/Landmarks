f0f0a671114bfde5e9b942396cca1dc43c26ccf4
Michael Starrett <mjstarrett@mbb.arizona.edu>
2020-01-09 13:12:27 -0700

    BrainAmpTrigger.cs succsessfully communitcating with BrainVisionRecorder and marking simulated traces

--- Assets/EEG Assets/BrainAmpTrigger.cs
+++ Assets/EEG Assets/BrainAmpTrigger.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class BrainAmpTrigger : MonoBehaviour {
 
     public int initialState = 0; // 0 in bits 0x00;
-    public int port = 18; // COM port number
+    public int state;
+    public int port = 3; // COM port number
     public int nTriggers = 255; // how many unique combinations (e.g., 8 bits = 256 possibilities)
 
     private SerialPort triggerBox;
@@ -18,8 +19,24 @@ public class BrainAmpTrigger : MonoBehaviour {
     void Start () {
         //initiate trigger box
         initiatePort();
+        state = initialState;
     }
 
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Return))
+    //    {
+    //        Test(UnityEngine.Random.Range(0, nTriggers));
+    //    }
+    //}
+
+    //public void Test(int trigger)
+    //{
+    //    Debug.Log(trigger);
+    //    bit[0] = (byte)trigger; // set the trigger number 
+    //    Debug.Log(bit[0]);
+    //    triggerBox.Write(bit, 0, 1);
+    //}
 
     // Coroutine to define square-wave pulse (with pause so we don't turn off too early)
     public IEnumerator Mark(int trigger)
@@ -37,23 +54,26 @@ public class BrainAmpTrigger : MonoBehaviour {
         bit[0] = (byte)initialState;
         triggerBox.Write(bit, 0, 1);
         Debug.Log("port data written: " + triggerBox.ReadByte());
+
+        state++;
     }
 
 
     public void initiatePort()
     {
         // Configure the triggerbox info
-        var portName = "COM" + (char)port; 
+        var portName = "COM" + port.ToString();
+        Debug.Log("Looking for TriggerBox on port " + portName);
         triggerBox = new SerialPort(portName);
 
         // Open the virtual serial port
         triggerBox.Open(); // open the port
-        Debug.Log("port open! " + portName);
+        Debug.Log("Successfully opened " + portName);
 
         // Set the port to an initial state
         bit[0] = (byte)initialState;
         triggerBox.Write(bit, 0, 1);
-        
+        Debug.Log("port data written: " + triggerBox.ReadByte());
     }
 
 
