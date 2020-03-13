59c24162ac940a9122b6ed4b24c02c7c68104478
mjstarrett <mjstarrett@gmail.com>
2020-03-13 14:50:09 -0700

    updated and removed references to depricated GUITexgt and GUIImage; replaced with UnityEngine.UI implementations

--- Assets/Landmarks/Scripts/ExperimentTasks/InstructionsTask.cs
+++ Assets/Landmarks/Scripts/ExperimentTasks/InstructionsTask.cs
@@ -46,7 +46,7 @@ public class InstructionsTask : ExperimentTask {
     public bool actionButtonOn = true;
     public string customButtonText = "";
 
-    private GUIText gui;
+    private Text gui;
 
     public bool restrictMovement = false; // MJS do we want to keep them still during this?
     public bool selfPaced = false; // can they press return to end the task?
@@ -80,11 +80,11 @@ public class InstructionsTask : ExperimentTask {
         Text canvas = avatar.GetComponent<Text>();
         hud.SecondsToShow = hud.InstructionDuration;
 
-        sgo.AddComponent<GUIText>();
+        sgo.AddComponent<Text>();
         sgo.hideFlags = HideFlags.HideAndDontSave;
         sgo.transform.position = new Vector3(0,0,0);
-        gui = sgo.GetComponent<GUIText>();
-        gui.pixelOffset = new Vector2( 20, Screen.height - 20);
+        gui = sgo.GetComponent<Text>();
+        // DEPRICATED IN UNITY 2019 // gui.pixelOffset = new Vector2( 20, Screen.height - 20);
         gui.font = instructionFont;
         gui.fontSize = instructionSize;
         gui.material.color = text_color;
