12e884a46276ceda6a54f1b7dbe9f922a7630735
LMdemo <mjstarrett@gmail.com>
2020-04-17 00:47:38 -0700

    updated compass

---
+++ Assets/Landmarks/Scripts/LM_Compass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LM_Compass : MonoBehaviour
+{
+    public GameObject pointer;
+    public float rotationSpeedMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pointer.transform.Rotate(transform.up, -1 * rotationSpeedMultiplier * Time.deltaTime);
+            Debug.Log("Trying to rotate left!");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pointer.transform.Rotate(transform.up,rotationSpeedMultiplier * Time.deltaTime);
+            Debug.Log("Trying to rotate left!");
+        }
+    }
+}
