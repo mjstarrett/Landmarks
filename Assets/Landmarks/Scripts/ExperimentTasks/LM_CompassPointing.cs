091c0ce6f429cad893c4eac634df07b8b8e3b85e
LMdemo <mjstarrett@gmail.com>
2020-05-11 15:39:30 -0700

    pointing working with incremented blocks

--- Assets/Landmarks/Scripts/ExperimentTasks/LM_CompassPointing.cs
+++ Assets/Landmarks/Scripts/ExperimentTasks/LM_CompassPointing.cs
@@ -30,9 +30,9 @@ public class LM_CompassPointing : ExperimentTask
     [Header("Task-specific Properties")]
     public Format format;
     public TextAsset sopText;
-    public LM_PermutedList sopList;
     public TextAsset jrdText;
-    public LM_PermutedList jrdList;
+    public TextAsset rvdText;
+    public LM_PermutedList listOfTriads;
     public bool randomStartRotation;
     public Vector3 compassPosOffset = new Vector3(0f, 0f, -2f);
     public Vector3 compassRotOffset = new Vector3(15f, 0f, 0f);
@@ -41,7 +41,8 @@ public class LM_CompassPointing : ExperimentTask
     private GameObject location; // standing at the...
     private GameObject orientation; // facing the...
     private GameObject target; // point to the...
-    private LM_Compass compass; 
+    private LM_Compass compass;
+    private float startAngle; // where is the compass pointer at the start?
     private float answer; // what should they say?
     private float response; // what did they say?
     private float signedError; // how far off were they to the right or left?
@@ -66,6 +67,15 @@ public class LM_CompassPointing : ExperimentTask
         manager.player.GetComponent<CharacterController>().enabled = false;
 
 
+        // ---------------------------------------------------------------------
+        // Configure the Task-independent variables ----------------------------
+        // ---------------------------------------------------------------------
+
+        questionItems = listOfTriads.GetCurrentSubset(); // Get the objects for this question
+        location = questionItems[0]; // where to the player is positioned (anchor 1)
+        orientation = questionItems[1]; // where the player is facing (anchor 2)
+        target = questionItems[2]; // where the player is estimating 
+
 
         // ---------------------------------------------------------------------
         // Configure the Task based on selected format -------------------------
@@ -74,30 +84,21 @@ public class LM_CompassPointing : ExperimentTask
         switch (format)
         {
             case Format.SOP:
-                // Set up the question for presentation
+                // Prepare SOP hud and question
                 hud.showEverything(); // configure the hud for the format
-                questionItems = sopList.permutedList[parentTask.repeatCount - 1]; // Get the objects for this question
-                location = questionItems[0];
-                orientation = avatar.GetComponentInChildren<LM_SnapPoint>().gameObject; 
-                target = questionItems[1];
                 formattedQuestion = string.Format(sopText.ToString(), target.name); // prepare to present the question
                 break;
 
             case Format.JRD:
-                // Set up the question for presentation
-                hud.showOnlyHUD(); // configure the hud for the format
-                questionItems = jrdList.permutedList[parentTask.repeatCount - 1]; // Get the objects for this question
-                location = questionItems[0];
-                orientation = questionItems[1];
-                target = questionItems[2];
-                formattedQuestion = string.Format(jrdText.ToString(), location.name, orientation.name, target.name); // prepare to present the question
+                // Prepare JRD hud and question
+                hud.showOnlyHUD(); 
+                formattedQuestion = string.Format(jrdText.ToString(), location.name, orientation.name, target.name); 
 
-                // Calculate the correct answer?
+                // Calculate the correct answer (done later for SOP)
                 answer = Vector3.SignedAngle(orientation.transform.position - location.transform.position,
                                             target.transform.position - location.transform.position, Vector3.up);
                 if (answer < 0) answer += 360;
                 Debug.Log("Answer is " + answer);
-
                 break;
 
             case Format.RVD:
@@ -117,8 +118,7 @@ public class LM_CompassPointing : ExperimentTask
             avatar.transform.position = location.transform.position; // move player to the pointing location
 
             // Point the player at the orientation for JRD or a random orientation for SOP start
-            if (format == Format.JRD) avatar.transform.LookAt(orientation.transform); 
-            else if (format == Format.SOP) avatar.transform.eulerAngles = new Vector3(0f, Random.Range(0f, 360 - Mathf.Epsilon));
+            avatar.transform.LookAt(orientation.transform); 
 
             // Reset the camera to be zeroed on the controller position (i.e. looking straight forward)
             avatar.GetComponentInChildren<Camera>().transform.localEulerAngles = Vector3.zero; // reset the camera
@@ -141,7 +141,8 @@ public class LM_CompassPointing : ExperimentTask
         compass.transform.localPosition = compassPosOffset; // adjust position
         compass.transform.localEulerAngles = compassRotOffset; // adjust rotation
         compass.transform.parent = compassparent; // send it back to its old parent to avoid funky movement effects
-        compass.ResetPointer(random:randomStartRotation);
+        compass.ResetPointer(random:randomStartRotation); // set the compass arrow to zero (or a random rotation)
+        startAngle = compass.pointer.transform.localEulerAngles.y;
         compass.gameObject.SetActive(false);
 
         // Put up the HUD
@@ -178,8 +179,9 @@ public class LM_CompassPointing : ExperimentTask
                     compass.transform.localEulerAngles = compassRotOffset; // adjust rotation
                     compass.transform.parent = compassparent; // send it back to its old parent to avoid funky movement effects
 
-                    // Calculate the correct answer?
-                    answer = Vector3.SignedAngle(orientation.transform.position - location.transform.position,
+                    // Calculate the correct answer (using the new oriented facing direction)
+                    var newOrientation = avatar.GetComponentInChildren<LM_SnapPoint>().gameObject;
+                    answer = Vector3.SignedAngle(newOrientation.transform.position - location.transform.position,
                                                 target.transform.position - location.transform.position, Vector3.up);
                     if (answer < 0) answer += 360;
                     Debug.Log("Answer is " + answer);
@@ -230,11 +232,13 @@ public class LM_CompassPointing : ExperimentTask
 
         if (trialLog.active)
         {
+            trialLog.AddData(transform.name + "_task", format.ToString());
             trialLog.AddData(transform.name + "_location", location.name);
             trialLog.AddData(transform.name + "_orientation", orientation.name);
             trialLog.AddData(transform.name + "_target", target.name);
-            trialLog.AddData(transform.name + "_answerCW", answer.ToString());
+            trialLog.AddData(transform.name + "_compassStartAngle", startAngle.ToString()); // record where we started the compass at
             trialLog.AddData(transform.name + "_responseCW", response.ToString());
+            trialLog.AddData(transform.name + "_answerCW", answer.ToString());
             trialLog.AddData(transform.name + "_signedError", signedError.ToString());
             trialLog.AddData(transform.name + "_absError", absError.ToString());
         }
@@ -245,8 +249,10 @@ public class LM_CompassPointing : ExperimentTask
     {
         base.endTask();
 
-        oriented = false;
-        compass.interactable = false;
+        listOfTriads.IncrementCurrentSubset(); // next set of targets
+
+        oriented = false; // reset for next SOP trial (if any)
+        compass.interactable = false; // shut off the compass object's function
         // Free Movement
         if (avatar.GetComponent<FirstPersonController>() != null) avatar.GetComponent<FirstPersonController>().enabled = true; // if using 1stPerson controller
         manager.player.GetComponent<CharacterController>().enabled = false;
