b6959ac66c3f9c44f589051c043203c49ff50ad6
mjstarrett <mjstarrett@gmail.com>
2022-02-11 21:09:54 -0800

    graphPrefs initial

---
+++ Assets/Landmarks/Scripts/ExperimentTasks/LM_GoTo.cs
@@ -0,0 +1,111 @@
+﻿/*
+    
+    Copyright (C) 2022 Michael J. Starrett Ambrose
+    University of California, Irvine
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LM_GoTo : ExperimentTask
+{
+    [Header("Task-specific Properties")]
+    public GameObject destination;
+    public float orientThreshold = 15.0f;
+
+    private ParticleSystem effect;
+    private bool atDestination;
+
+    public override void startTask()
+    {
+        TASK_START();
+
+        // LEAVE BLANK
+    }
+
+
+    public override void TASK_START()
+    {
+        if (!manager) Start();
+        base.startTask();
+
+        if (skip)
+        {
+            log.log("INFO    skip task    " + name, 1);
+            return;
+        }
+
+
+        hud.SecondsToShow = 0; // we don't need a hud
+
+        if (destination.GetComponentInChildren<ParticleSystem>() != null) effect = destination.GetComponentInChildren<ParticleSystem>();
+        destination.SetActive(true); // show the destination if it's hidden
+        if (effect != null) effect.Play(true); // start particles if we have them
+        hud.showOnlyHUD(); // FIXME for now. Eventually you will need to add a hud.ShowTerrain, or something like that.
+        manager.environment.SetActive(false);
+    }
+
+
+    public override bool updateTask()
+    {
+        // Is the player at and aligned with the destination?
+        if (atDestination & Mathf.Abs(Mathf.DeltaAngle(avatar.transform.eulerAngles.y, destination.transform.eulerAngles.y)) < orientThreshold)
+        {
+            if (manager.usingVR)
+            {
+                if (vrInput.TriggerButton.stateDown)
+                {
+                    log.log("INPUT_EVENT    Player Arrived at Destination    1", 1);
+                    return true;
+                }
+            }
+            else if (Input.GetButtonDown("Return") | Input.GetKeyDown(KeyCode.Return))
+            {
+                log.log("INPUT_EVENT    Player Arrived at Destination    1", 1);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public override void endTask()
+    {
+        TASK_END();
+
+        // LEAVE BLANK
+    }
+
+
+    public override void TASK_END()
+    {
+        base.endTask();
+
+        // WRITE TASK EXIT CODE HERE
+        destination.SetActive(false);
+        manager.environment.SetActive(true);
+        hud.SecondsToShow = hud.GeneralDuration;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            if (effect != null) effect.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            atDestination = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            if (effect != null) effect.Play(true);
+            atDestination = false;
+        }
+    }
+
+}
---
+++ Assets/Landmarks/Scripts/ExperimentTasks/LM_ToggleObjects.cs
@@ -0,0 +1,75 @@
+﻿/*
+    LM Dummy
+       
+    Attached object holds task components that need to be effectively ignored 
+    by Tasklist but are required for the script. Thus the object this is 
+    attached to can be detected by Tasklist (won't throw error), but does nothing 
+    except start and end.   
+
+    Copyright (C) 2019 Michael J. Starrett
+
+    Navigate by StarrLite (Powered by LandMarks)
+    Human Spatial Cognition Laboratory
+    Department of Psychology - University of Arizona   
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LM_ToggleObjects : ExperimentTask
+{
+    [Header("Task-specific Properties")]
+    public List<GameObject> objsToToggle;
+    public bool setStateOn;
+
+    public override void startTask()
+    {
+        TASK_START();
+
+        // LEAVE BLANK
+    }
+
+
+    public override void TASK_START()
+    {
+        if (!manager) Start();
+        base.startTask();
+
+        if (skip)
+        {
+            return;
+        }
+        // WRITE TASK STARTUP CODE HERE
+        foreach (var obj in objsToToggle)
+        {
+            obj.SetActive(setStateOn);
+        }
+
+    }
+
+
+    public override bool updateTask()
+    {
+        return true;
+
+        // WRITE TASK UPDATE CODE HERE
+    }
+
+
+    public override void endTask()
+    {
+        TASK_END();
+
+        // LEAVE BLANK
+    }
+
+
+    public override void TASK_END()
+    {
+        base.endTask();
+
+        // WRITE TASK EXIT CODE HERE
+    }
+
+}
