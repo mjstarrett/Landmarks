72018e8b9de29bbd5a18be44283b698c6fa3ec79
Mike Starrett Ambrose <mjstarrett@gmail.com>
2023-10-25 11:06:38 -0700

    added missing reference finder and fixed single-scene loading in builds

---
+++ Assets/Editor/missingReferenceFinder.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+public class MissingReferencesFinder : MonoBehaviour 
+{
+    [MenuItem("Tools/Show Missing Object References in scene", false, 50)]
+    public static void FindMissingReferencesInCurrentScene()
+    {
+        var objects = GetSceneObjects();
+        for (int iscene = 0; iscene < EditorSceneManager.sceneCount; iscene++)
+        {
+            var thisScene = EditorSceneManager.GetSceneAt(iscene);
+            FindMissingReferences(thisScene.name, objects);
+        }
+    }
+ 
+    [MenuItem("Tools/Show Missing Object References in all scenes", false, 51)]
+    public static void MissingSpritesInAllScenes()
+    {
+        foreach (var scene in EditorBuildSettings.scenes.Where(s => s.enabled))
+        {
+            for (int iscene = 0; iscene < EditorSceneManager.sceneCount; iscene++)
+            {
+                var thisScene = EditorSceneManager.GetSceneAt(iscene);
+                EditorSceneManager.OpenScene(thisScene.path);
+                FindMissingReferences(thisScene.path, GetSceneObjects());
+            }
+        }
+    }
+ 
+    [MenuItem("Tools/Show Missing Object References in assets", false, 52)]
+    public static void MissingSpritesInAssets()
+    {
+        var allAssets = AssetDatabase.GetAllAssetPaths();
+        var objs = allAssets.Select(a => AssetDatabase.LoadAssetAtPath(a, typeof(GameObject)) as GameObject).Where(a => a != null).ToArray();
+         
+        FindMissingReferences("Project", objs);
+    }
+ 
+    private static void FindMissingReferences(string context, GameObject[] objects)
+    {
+        foreach (var go in objects)
+        {
+            var components = go.GetComponents<Component>();
+             
+            foreach (var c in components)
+            {
+                if (!c)
+                {
+                    Debug.LogError("Missing Component in GO: " + FullPath(go), go);
+                    continue;
+                }
+                 
+                SerializedObject so = new SerializedObject(c);
+                var sp = so.GetIterator();
+                 
+                while (sp.NextVisible(true))
+                {
+                    if (sp.propertyType == SerializedPropertyType.ObjectReference)
+                    {
+                        if (sp.objectReferenceValue == null
+                            && sp.objectReferenceInstanceIDValue != 0)
+                        {
+                            ShowError(context, go, c.GetType().Name, ObjectNames.NicifyVariableName(sp.name));
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
+    private static GameObject[] GetSceneObjects()
+    {
+        return Resources.FindObjectsOfTypeAll<GameObject>()
+            .Where(go => string.IsNullOrEmpty(AssetDatabase.GetAssetPath(go))
+                   && go.hideFlags == HideFlags.None).ToArray();
+    }
+     
+    private const string err = "Missing Ref in: [{3}]{0}. Component: {1}, Property: {2}";
+     
+    private static void ShowError (string context, GameObject go, string c, string property)
+    {
+        Debug.LogError(string.Format(err, FullPath(go), c, property, context), go);
+    }
+     
+    private static string FullPath(GameObject go)
+    {
+        return go.transform.parent == null
+            ? go.name
+                : FullPath(go.transform.parent.gameObject) + "/" + go.name;
+    }
+}
